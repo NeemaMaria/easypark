548814f89df969ed391b8cd6463fdd576c816748
Neema_Maria <marianeema02@gmail>
2024-02-01 18:01:51 +0300

    more changes

--- lib/Presentation/home.dart
+++ lib/Presentation/home.dart
@@ -71,6 +71,129 @@ class _homeState extends State<home> {
                   ),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 39, 39, 39),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: Padding(
+                    padding:const EdgeInsets.all(16.0),
+                    child:Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children:[
+                            Flexible(
+                              child: Text(
+                                "Parking at Urban Parking Shelton Street Car Park, WC2H",
+                                style: TextStyle(
+                                  color:Color.fromARGB(255, 218, 218, 218),
+                                  fontSize: 24
+                                )),
+                            ),
+                              Container(
+                                height:40,
+                                width:40,
+                                decoration: BoxDecoration(
+                                  color:Color.fromARGB(255, 218, 218,218),
+                                  borderRadius:BorderRadius.all(Radius.circular(5)),
+                                ),
+                                child: const Icon(
+                                  Icons.share_location,
+                                  size: 30,
+                                ),
+                              ),
+                          ]
+                        ),
+                        SizedBox(height:8.0),
+                        Divider(
+                          color: Colors.grey,
+                        ),
+                        SizedBox(height:10),
+                        
+         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Enter After",
+                                  style:TextStyle(color:Colors.grey),),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(
+                                    "04 April at 10:30pm"
+                                    ,
+                                  style: TextStyle(
+                                    color:Color.fromARGB(255, 218, 218, 218),
+                                    fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height:8.0),
+                                   Text(
+                                  "Duration",
+                                  style:TextStyle(color:Colors.grey),),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(
+                                    "24 Hours"
+                                    ,
+                                  style: TextStyle(
+                                    color:Color.fromARGB(255, 218, 218, 218),
+                                    fontWeight: FontWeight.bold),
+                                  ),
+                              ],
+                            ),
+                             Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                               Text(
+                                  "Exit Before",
+                                  style:TextStyle(color:Colors.grey),),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(
+                                    "05 April at 10:30pm"
+                                    ,
+                                  style: TextStyle(
+                                    color:Color.fromARGB(255, 218, 218, 218),
+                                    fontWeight: FontWeight.bold),),
+                                    SizedBox(height:8),
+                                    Text(
+                                  "Booking Price",
+                                  style:TextStyle(color:Colors.grey),),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(
+                                    "\$52.00",
+                                    
+                                  style: TextStyle(
+                                    color:Color.fromARGB(255, 218, 218, 218),
+                                    fontWeight: FontWeight.bold),
+                                  ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height:10,),
+                        Transform.scale(
+                          scaleY: 0.7,
+                          child: Image.asset(
+                            "assets/barcode.png"
+                            ))
+                      
+                      ],
+                    )
+                  ),
+                ),
+              )
           ],
         ),
         ),
