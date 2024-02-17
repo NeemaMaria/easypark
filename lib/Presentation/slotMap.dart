85337a9f2e494b112490586944fe1d10d9742e8e
edgarmuyomba <edgarmatthew247@gmail.com>
2024-02-17 17:35:06 +0300

    trial one

--- lib/Presentation/slotMap.dart
+++ lib/Presentation/slotMap.dart
@@ -3,6 +3,7 @@ import 'package:easypark/models/ParkingDetails.dart';
 import 'package:easypark/models/Slot.dart';
 import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
+import 'package:mapbox_gl/mapbox_gl.dart';
 import "../variables.dart";
 
 class SlotMap extends StatefulWidget {
@@ -14,20 +15,26 @@ class SlotMap extends StatefulWidget {
 }
 
 class _SlotMapState extends State<SlotMap> {
-
   Slot slot = Slot();
   ParkingDetails parking_lot = ParkingDetails();
 
   bool loading = true;
 
+  late CameraPosition _initialCameraPosition;
+  late MapboxMapController controller;
+
+  LatLng currentLatLng = LatLng(lat, lon);
+
   Future<Slot> slotDetails() async {
-    var response = await http.get(Uri.parse("http://$server:8000/slot_details/${widget.uuid}/"));
+    var response = await http
+        .get(Uri.parse("http://$server:8000/slot_details/${widget.uuid}/"));
     var json = jsonDecode(response.body);
     return Slot.fromJson(json);
   }
 
   Future<ParkingDetails> parkingDetails(uuid) async {
-    var response = await http.get(Uri.parse("http://$server:8000/parking_lot_details/$uuid/"));
+    var response = await http
+        .get(Uri.parse("http://$server:8000/parking_lot_details/$uuid/"));
     var json = jsonDecode(response.body);
     return ParkingDetails.fromJson(json);
   }
@@ -43,6 +50,8 @@ class _SlotMapState extends State<SlotMap> {
         parking_lot = value;
       });
     });
+    // location details
+    _initialCameraPosition = CameraPosition(target: currentLatLng, zoom: 15);
   }
 
   @override
--- lib/variables.dart
+++ lib/variables.dart
@@ -8,3 +8,6 @@ String user_id = "xDI14RQJa6r2Wq8Y0jgp99wVMMfs";
 String user_name = "John";
 
 String slot_id = "a132d18f-d807-450b-bdb2-4b35780ff181";
+
+String public_token = "pk.eyJ1IjoiZWRnYXJtYXR0aGV3IiwiYSI6ImNsc2lxcG4zcTF6MGoybW1oYXF3a214NWEifQ.sOeAWXNngsxPBE1TbHmudw";
+String secret_token = "sk.eyJ1IjoiZWRnYXJtYXR0aGV3IiwiYSI6ImNsc3E2MzV3YjA2dWgybHFpNjd4Ymw4MmEifQ.2N4f_DMmsG6MbFz0n4Porg";
