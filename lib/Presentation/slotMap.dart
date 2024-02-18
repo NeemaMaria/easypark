e1dd0d129ef1b24a2e6e3211e05dfd9656fdc7a2
edgarmuyomba <edgarmatthew247@gmail.com>
2024-02-18 12:55:41 +0300

    extra details

--- lib/Presentation/slotMap.dart
+++ lib/Presentation/slotMap.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
+import 'package:easypark/Presentation/userSessions.dart';
 import 'package:easypark/mapbox/directions_handler.dart';
 import 'package:easypark/models/ParkingDetails.dart';
 import 'package:easypark/models/Slot.dart';
 import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
+import 'package:lottie/lottie.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import "../variables.dart";
 
@@ -86,6 +88,46 @@ class _SlotMapState extends State<SlotMap> {
     return ParkingDetails.fromJson(json);
   }
 
+  Future<void> park() async {
+    var response = await http.post(Uri.parse(
+        "http://$server:8000/park_in_slot/${parking_lot.uuid}/${slot.uuid}/$user_id/"));
+    if (response.statusCode == 200) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => UserSessions()));
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          backgroundColor: Colors.green[500],
+          content: const Text("Parking Session Active")));
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              backgroundColor: Colors.red[400],
+              title: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Error",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  Icon(Icons.warning_amber_rounded, color: Colors.white)
+                ],
+              ),
+              content: const Text(
+                "Something went wrong. Try again!",
+                style: TextStyle(color: Colors.white),
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text("OK", style: TextStyle(color: Colors.white)))
+              ],
+            );
+          });
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -107,32 +149,130 @@ class _SlotMapState extends State<SlotMap> {
       });
       slotPosition = CameraPosition(
           target: LatLng(slot.latitude!, slot.longitude!), zoom: 15);
+      setState(() {
+        loading = false;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Slot Map"),
-      ),
-      body: SafeArea(
-          child: Stack(
-        children: [
-          SizedBox(
-            height: 0.9 * MediaQuery.of(context).size.height,
-            child: MapboxMap(
-              accessToken: secret_token,
-              initialCameraPosition: _initialCameraPosition,
-              onMapCreated: _onMapCreated,
-              onStyleLoadedCallback: _onStyleLoadedCallback,
-              myLocationEnabled: true,
-              myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
-              minMaxZoomPreference: const MinMaxZoomPreference(14, 17),
-            ),
-          )
-        ],
-      )),
+      body: loading
+          ? Center(child: CircularProgressIndicator())
+          : SafeArea(
+              child: Stack(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: MapboxMap(
+                    accessToken: secret_token,
+                    initialCameraPosition: _initialCameraPosition,
+                    onMapCreated: _onMapCreated,
+                    onStyleLoadedCallback: _onStyleLoadedCallback,
+                    myLocationEnabled: true,
+                    myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
+                    minMaxZoomPreference: const MinMaxZoomPreference(14, 17),
+                  ),
+                ),
+                Column(children: [
+                  SizedBox(height: 0.01 * deviceHeight),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              color: Colors.green[400],
+                              borderRadius: BorderRadius.circular(100)),
+                          child: IconButton(
+                              onPressed: () => Navigator.pop(context),
+                              icon:
+                                  Icon(Icons.arrow_back, color: Colors.white)),
+                        ),
+                        const SizedBox(width: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              color: Colors.green[400]),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0, vertical: 8.0),
+                            child: Text(
+                              slot.slot_number!,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(child: SizedBox()),
+                  slot.occupied!
+                      ? Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 150),
+                              InkWell(
+                                onTap: () => park(),
+                                child: Container(
+                                  height: 45,
+                                  width: 0.35 * deviceWidth,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      color: Colors.blue[400],
+                                      borderRadius: BorderRadius.circular(25)),
+                                  child: const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 8.0, horizontal: 16.0),
+                                    child: Text(
+                                      "Park",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: Column(
+                            children: [
+                              Lottie.asset(height: 100, "assets/driving.json"),
+                              SizedBox(height: 16.0),
+                              InkWell(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(25)),
+                                  child: const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 20.0, vertical: 8.0),
+                                    child: Text(
+                                      "On your way...",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                ])
+              ],
+            )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           controller.animateCamera(
