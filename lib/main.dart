482b0132424e284995239011d90d876d89dcba7a
edgarmuyomba <edgarmatthew247@gmail.com>
2024-02-18 11:51:55 +0300

    location services

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:easypark/Presentation/home.dart';
 import 'package:easypark/Presentation/slotMap.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 import './variables.dart';
 
 void main() async {
@@ -15,6 +16,13 @@ class EasyPark extends StatefulWidget {
 }
 
 class _EasyParkState extends State<EasyPark> {
+
+  @override
+  void initState() {
+    super.initState();
+    handleLocationPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,5 +30,21 @@ class _EasyParkState extends State<EasyPark> {
       debugShowCheckedModeBanner: false,
       home: SlotMap(uuid: slot_id)
     );
+  }
+}
+
+Future handleLocationPermissions() async {
+  Location _location = Location();
+  bool? _serviceEnabled;
+  PermissionStatus? _permissionGranted;
+
+  _serviceEnabled = await _location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await _location.requestService();
+  }
+
+  _permissionGranted = await _location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await _location.requestPermission();
   }
 }
