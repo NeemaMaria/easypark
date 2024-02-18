347355f5870f7176f2a3f13f6f159e5bb9ce90ff
edgarmuyomba <edgarmatthew247@gmail.com>
2024-02-18 13:07:38 +0300

    somethings wrong with the polylines

--- lib/variables.dart
+++ lib/variables.dart
@@ -2,7 +2,7 @@ double lat = 0.2627623;
 double lon = 32.5415196;
 String location_name = "Bunamwaya, Zzana";
 
-String server = "10.0.2.2";
+String server = "192.168.233.96";
 
 String user_id = "xDI14RQJa6r2Wq8Y0jgp99wVMMfs";
 String user_name = "John";
